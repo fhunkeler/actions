422ff492052bd864b2cc106345327de874f9253c
Florent Hunkeler <florent.hunkeler@open-groupe.com>
2021-11-17 11:23:53 +0100

    fail test

--- front/src/App.test.js
+++ front/src/App.test.js
@@ -14,5 +14,5 @@ import App from './App';
 
 it('renders welcome message', () => {
   render(<App />);
-  expect(screen.getByText('Hello')).toBeInTheDocument();
+  expect(screen.getByText('hello')).toBeInTheDocument();
 });
